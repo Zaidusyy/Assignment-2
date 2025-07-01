2a132fbcbed816986b80b5847481367372c3a53a
BhoomiJaiswal0 <bhoomijais001@gmail.com>
2025-07-02 03:00:42 +0530

    Update solution.java

--- submissions/BhoomiJaiswal/FibonacciSeries/solution.java
+++ submissions/BhoomiJaiswal/FibonacciSeries/solution.java
@@ -1,1 +1,22 @@
+// solution.java
+import java.util.Scanner;
 
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        sc.close();
+    }
+}
